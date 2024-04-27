416e8da93a66f20ad7ff4e19d7bebe22a9a08e74
Manel1804 <abakhouchmanel@gmail.com>
2024-04-27 15:27:16 +0100

    deletecommentdolphon

--- src/entities/Dolphin.java
+++ src/entities/Dolphin.java
@@ -4,24 +4,24 @@ public class Dolphin extends Aquatic{
 package entities;
 
     public class Dolphin extends Aquatic{
-        //att
+
         private float swimmingSpeed;
 
-        //constructeur
+
         public Dolphin(){}
         public Dolphin(String fam, String na, int ag, boolean mammal, String habitat, float swimmingSpeed){
             super(fam, na, ag, mammal, habitat);
             this.swimmingSpeed = swimmingSpeed;
         }
 
-        //methods
+
 
         @Override
         public void swim() {
             System.out.println("This dolphin is swimming.");
         }
 
-        //diplay
+
         @Override
         public String toString() {
             return "Dolphin = [family : "+ this.family + ",\n" +
