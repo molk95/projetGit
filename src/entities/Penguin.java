fdd93c6ebac478a661d41514de4df1db63623613
Manel1804 <abakhouchmanel@gmail.com>
2024-04-27 13:27:00 +0100

    Penguin

--- src/entities/Penguin.java
+++ src/entities/Penguin.java
@@ -1,5 +1,33 @@
 package entities;
 
-public class Penguin extends Aquatic{
 
-}
+    public class Penguin extends Aquatic{
+        //att
+        private float swimmingDepth;
+
+        //constructeur
+        public Penguin(){}
+        public Penguin(String fam, String na, int ag, boolean mammal, String habitat, float swimmingDepth){
+            super(fam, na, ag, mammal, habitat);
+            this.swimmingDepth = swimmingDepth;
+        }
+        //methods
+
+        @Override
+        public void swim() {
+            System.out.println("This penguin is swimming");
+        }
+
+        //diplay
+        @Override
+        public String toString() {
+            return "Penguin = [family : "+ this.family + ",\n" +
+                    "name : "+ this.name + ",\n" +
+                    "age : "+ this.age + ",\n" +
+                    "mammal : "+ this.mammal + ",\n" +
+                    "habitat : "+ this.habitat + ",\n" +
+                    "Depth : "+ this.swimmingDepth + "]"
+                    ;
+        }
+    }
+
--- src/entities/Zoo.java
+++ src/entities/Zoo.java
@@ -5,4 +5,7 @@ import java.util.Arrays;
 public class Zoo {
 
 
+
+
+
 }
